6fc3dceef27cec1de1e797cd9e48d9c2f7b85ab1
straversi <straversi@google.com>
2019-08-29 19:22:36 -0700

    pr feedback: use bespoke data structure for easier readable representation of reflected attributes

--- src/lib/reflected-attributes.ts
+++ src/lib/reflected-attributes.ts
@@ -12,22 +12,67 @@
  * http://polymer.github.io/PATENTS.txt
  */
 
-const reflectedAttributes: Map<string, Map<string, string>> = new Map([
-  ['input', new Map([
-    ['value', 'value']
-  ])]
-]);
+/**
+ * Elements and their special properties that should be reflected to attributes
+ * when set.
+ *
+ * Each item in the array takes the following format:
+ *
+ * Item 0: Element name or array of element names.
+ * Rest of items:
+ *    Array of [property name, reflected attribute name]
+ *    or
+ *    property name (if reflected attribute name is identical).
+ */
+const reflectedAttributesSource: (string|string[])[][] = [
+  [['input', 'select'], 'value'],
+  ['*', ['classname', 'class'], 'id']
+];
 
-const reflectedGlobalAttributes: Map<string, string> = new Map([
-  ['classname', 'class'],
-  ['id', 'id'],
-]);
-
-export const reflectedAttributeName = (tag: string, property: string): string|undefined => {
-  const attributes = reflectedAttributes.get(tag);
-  if (attributes !== undefined && attributes.has(property)) {
-    return attributes.get(property);
+/**
+ * Construct reflectedAttributes as nested maps for efficient lookup.
+ */
+const reflectedAttributes = new Map<string, Map<string, string>>();
+const addPropertyForElement = (elementName: string, propertyName: string, attributeName: string) => {
+  if (reflectedAttributes.has(elementName)) {
+    reflectedAttributes.get(elementName)!.set(propertyName, attributeName);
   } else {
-    return reflectedGlobalAttributes.get(property);
+    reflectedAttributes.set(elementName, new Map([[propertyName, attributeName]]));
+  }
+}
+const addPropertiesForElement = (elementName: string, propertyNames: Array<string|Array<string>>) => {
+  for (const propertyName of propertyNames) {
+    if (propertyName instanceof Array) {
+      // Property has a different attribute name.
+      addPropertyForElement(elementName, propertyName[0], propertyName[1]);
+    } else {
+      addPropertyForElement(elementName, propertyName, propertyName)
+    }
+  }
+}
+for (const entry of reflectedAttributesSource) {
+  if (entry[0] instanceof Array) {
+    for (const elementName of entry[0]) {
+      addPropertiesForElement(elementName, entry.slice(1))
+    }
+  } else {
+    addPropertiesForElement(entry[0] as string, entry.slice(1))
+  }
+}
+
+/**
+ * Return the attribute name that reflects from the given property
+ * name on the given element.
+ *
+ * Example: for all elements, the property 'className' reflects to
+ * the 'class' attribute, so:
+ * reflectedAttributeName('div', 'className') returns 'class'
+ */
+export const reflectedAttributeName = (elementName: string, propertyName: string): string|undefined => {
+  const attributes = reflectedAttributes.get(elementName);
+  if (attributes !== undefined && attributes.has(propertyName)) {
+    return attributes.get(propertyName);
+  } else {
+    return reflectedAttributes.get('*')!.get(propertyName);
   }
 }
