e01b9a40d6d5b1efc95c54e319851ce5a729d949
Justin Fagnani <justinfagnani@google.com>
2020-05-19 11:57:02 -0700

    Use opcodes to render (#55)

--- src/lib/lit-element-renderer.ts
+++ src/lib/lit-element-renderer.ts
@@ -35,9 +35,7 @@ export class LitElementRenderer implements ElementRenderer {
     yield '</shadow-root>';
   }
 
-  *renderStyles(
-    _elementClass: Constructor<HTMLElement>
-  ): Iterator<string> {
+  *renderStyles(_elementClass: Constructor<HTMLElement>): Iterator<string> {
     const scopedStyles = [];
     for (const [tagName, definition] of (customElements as any).__definitions) {
       const styles = [(definition.ctor as any).styles].flat(Infinity);
